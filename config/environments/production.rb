0aebeba2f78aa8b90c5c372aecdd64da4b0aa525
Seamus Abshere <seamus@abshere.net>
2011-01-11 17:05:20 -0600

    serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Yaktrak::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  # sabshere 1/12/11 per heroku
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
