ca6669f9b50c804b44887ead9afbf8d898555d6c
Andy Rossmeissl <andy@rossmeissl.net>
2011-01-18 12:30:08 -0500

    Alternatively, pull FedEx details from yml

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Yaktrak::Application.initialize!
 
-FEDEX = { # pull from env
-  :key => ENV['FEDEX_KEY'],
-  :password => ENV['FEDEX_PASSWORD'],
-  :account => ENV['FEDEX_ACCOUNT'],
-  :meter => ENV['FEDEX_METER']
-}
+if File.exist?(File.join(Rails.root, 'config', 'fedex.yml'))
+  FEDEX = YAML::load(File.open(File.join(Rails.root, 'config', 'fedex.yml')))
+else
+  FEDEX = { # pull from env
+    :key => ENV['FEDEX_KEY'],
+    :password => ENV['FEDEX_PASSWORD'],
+    :account => ENV['FEDEX_ACCOUNT'],
+    :meter => ENV['FEDEX_METER']
+  }
+end
