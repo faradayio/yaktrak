3145796a0e5c88046c5ab624e05f186dd0fd49c5
Derek Kastner <dkastner@gmail.com>
2011-01-21 14:46:24 -0500

    passing entire URI to mongo

--- config/initializers/tronprint.rb
+++ config/initializers/tronprint.rb
@@ -1,12 +1,8 @@
 require 'tronprint'
 require 'uri'
 
-puts 'configuring tronprint'
-uri = URI.parse ENV['MONGOHQ_URL']
 Tronprint.aggregator_options = {
   :adapter => :mongodb,
-  :host => uri.host,
-  :port => uri.port,
-  :db => uri.path.gsub('/','_'),
+  :uri => URI.parse ENV['MONGOHQ_URL'],
   :collection => 'tronprint'
 }
