336451e12cdf8d82f183cda78d319dd871311dd5
Derek Kastner <dkastner@gmail.com>
2011-01-21 15:51:27 -0500

    syntax fix

--- config/initializers/tronprint.rb
+++ config/initializers/tronprint.rb
@@ -3,6 +3,6 @@ require 'uri'
 
 Tronprint.aggregator_options = {
   :adapter => :mongodb,
-  :uri => URI.parse ENV['MONGOHQ_URL'],
+  :uri => URI.parse(ENV['MONGOHQ_URL']),
   :collection => 'tronprint'
 }
